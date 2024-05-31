a551620cfac2b83887845ec6b33283c1c8ebaab8
Gabriel Iope <gabriel.r.iope@gmail.com>
2024-05-31 01:19:24 -0300

    feature: Base de FrontEnd da tela de login e cadastro.

---
+++ src/main/resources/web/script.js
@@ -0,0 +1,12 @@
+function showLogin() {
+    document.getElementById('cadastro').classList.add('hidden');
+     
+    document.getElementById('login').classList.remove('hidden');     
+}
+
+function showCadastro() {
+    document.getElementById('login').classList.add('hidden');
+
+     
+    document.getElementById('cadastro').classList.remove('hidden');
+}
